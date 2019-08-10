06bd9c0cf94af768c27325f4a613f0369baec010
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-10 11:51:42 -0400

    Add project files.

---
+++ IfStatementProjectPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IfStatementProjectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
